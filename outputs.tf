88838407a5e3d3a2eff936320923ddf7725bf412
WAYNEisBATMAN <p.jangraaa@gmail.com>
2025-10-03 23:19:02 +0530

    vsc

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
-output "nginx_access" {
-  description = "Ways to access the Nginx container"
-  value       = "Local access (from inside VM/EC2): http://localhost:${var.external_port}\nExternal access (from your system): http://<your-vm/ec2-public-ip>:${var.external_port}"
+output "nginx_local_url" {
+  description = "Local access URL"
+  value       = "http://localhost:${var.external_port}"
 }
 
-
+output "nginx_external_url" {
+  description = "External access URL (replace with actual IP)"
+  value       = "http://<your-vm/ec2-public-ip>:${var.external_port}"
+}
